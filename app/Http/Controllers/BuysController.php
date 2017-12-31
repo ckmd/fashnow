08aed079d0c0fb1a2fe1f2ef3bb1045ec863bae4
miftahun <miftahunajat@gmail.com>
2017-12-31 15:18:06 +0700

    Tambah Index Tambah Keranjang Layout

---
+++ app/Http/Controllers/BuysController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BuysController extends Controller
+{
+    public function showKeranjang(){
+        return view('post.product_summary');
+      }
+}
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,26 @@
+<a id="smallScreen" data-target="#topMenu" data-toggle="collapse" class="btn btn-navbar">
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+</a>
+  <div class="navbar-inner">
+  	<!--logo-->
+    <a class="brand" href="index.html">FASHNOW</a>
+        	<a class="brand">Online shop terpercaya</a>
+
+    <ul id="topMenu" class="nav pull-right">
+    <li class="">
+    </li>
+    <li class="">
+    	<a>Temukan kami di </a>
+    </li>
+	 <li class="">
+		<a href="#"><img width="30" height="30" src="themes/images/facebook.png" title="facebook" alt="facebook"/></a></li>
+	<li class="">
+		<a href="#"><img width="30" height="30" src="themes/images/twitter.png" title="twitter" alt="twitter"/></a></li>
+	<li class="">
+		<a href="#"><img width="30" height="30" src="themes/images/youtube.png" title="youtube" alt="youtube"/></a></li>
+    </ul>
+  </div>
+</div>
+</div>

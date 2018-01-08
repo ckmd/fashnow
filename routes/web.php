ecbb325be8abcabff17b2c7ef0d9ff9d330a779d
miftahun <miftahunajat@gmail.com>
2018-01-08 21:17:32 +0700

    Sudah bisa menambahkan ke keranjang dari tombol add to cart

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/products/{id}','ProductsController@showDetail');
 Route::post('/products/{id}','ProductsController@calculate');
 Route::get('/confirmation','BuysController@confirmation');
 //Route::get('/home', 'HomeController@index');
-Route::post('/products','ProductsController@showPerCategories');
+Route::post('/products','ProductsController@tambahStock');
 Route::post('/succeess','BuysController@prosesTransaksi');
 
 Route::get('/products', function()
